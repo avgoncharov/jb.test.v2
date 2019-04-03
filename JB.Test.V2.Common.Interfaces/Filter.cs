4dd88a20d2e515a08ad290cdb4352ac86796afcd
andrey <goncharov.andrey.v@gmail.com>
2019-04-03 21:22:32 +0300

    start new verions.

---
+++ JB.Test.V2.Common.Interfaces/Filter.cs
@@ -0,0 +1,25 @@
+﻿namespace JB.Test.V2.Common.Interfaces
+{
+	/// <summary>
+	/// Class of a filter for package searching.
+	/// </summary>
+	public sealed class Filter
+	{
+		/// <summary>
+		/// Gets / sets id template.
+		/// </summary>
+		public string Id { get; set; }
+
+
+		/// <summary>
+		/// Gets / sets version template.
+		/// </summary>
+		public string Version { get; set; }
+
+
+		/// <summary>
+		/// Gets / sets description template.
+		/// </summary>
+		public string Description { get; set; }
+	}
+}
---
+++ JB.Test.V2.Common.Interfaces/IPackage.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JB.Test.V2.Common.Interfaces
+{
+	/// <summary>
+	/// Interface of a package.
+	/// </summary>
+	public interface IPackage
+	{
+		/// <summary>
+		/// Gets Id of the package.
+		/// </summary>
+		string Id { get; }
+
+
+		/// <summary>
+		/// Gets version of the package.
+		/// </summary>
+		string Version { get; }
+
+
+		/// <summary>
+		/// Gets description of the package.
+		/// </summary>
+		string Description { get; }
+
+		/// <summary>
+		/// Gets metadata of the package.
+		/// </summary>
+		string Metadata { get; }
+
+		/// <summary>
+		/// Gets stream of the package data.
+		/// </summary>
+		Task<Stream> OpenAsync(CancellationToken token);
+	}
+}
