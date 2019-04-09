f5fc7ae56e5d2edf067201da24b97d84801e4312
Goncharov Andrey <goncharov.andrey.v@gmail.com>
2019-04-09 11:05:41 +0300

    Update QueryController.cs

--- JB.Test.V2.Web/Controllers/QueryController.cs
+++ JB.Test.V2.Web/Controllers/QueryController.cs
@@ -10,19 +10,5 @@ namespace JB.Test.V2.Web.Controllers
 	[RoutePrefix("query")]
 	public class QueryController : ApiController
 	{
-	}
-
-	/*
-	 * {
-			"@id": "http://localhost:65370/v3/registration3-gz/",
-			"@type": "RegistrationsBaseUrl/3.4.0",
-			"comment": "Base URL of Azure storage where NuGet package registration info is stored in GZIP format. This base URL does not include SemVer 2.0.0 packages."
-		},
-		{
-			"@id": "http://localhost:65370/v3/registration3-gz-semver2/",
-			"@type": "RegistrationsBaseUrl/3.6.0",
-			"comment": "Base URL of Azure storage where NuGet package registration info is stored in GZIP format. This base URL includes SemVer 2.0.0 packages."
-		},
-	 *
-	 */
+	}	
 }
